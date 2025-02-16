007299ef2e070df08b32a3f4278cf9bbfa01a31e
mafutka <mafutka@gmail.com>
2025-02-16 21:24:31 +0200

    task-2

--- js/task-2.js
+++ js/task-2.js
@@ -1,6 +1,41 @@
+function calcAverageCalories(days) {
+    if (days.length === 0) {
+        return 0; 
+    }
+    let totalCcal = 0;
+    for (let i = 0; i < days.length; i++) {
+        totalCcal += days[i].calories;
+    }
+    return totalCcal / days.length;
+}
 
+console.log(
+    calcAverageCalories([
+      { day: "monday", calories: 3010 },
+      { day: "tuesday", calories: 3200 },
+      { day: "wednesday", calories: 3120 },
+      { day: "thursday", calories: 2900 },
+      { day: "friday", calories: 3450 },
+      { day: "saturday", calories: 3280 },
+      { day: "sunday", calories: 3300 }
+    ])
+  );
 
+  console.log(
+    calcAverageCalories([
+      { day: "monday", calories: 2040 },
+      { day: "tuesday", calories: 2270 },
+      { day: "wednesday", calories: 2420 },
+      { day: "thursday", calories: 1900 },
+      { day: "friday", calories: 2370 },
+      { day: "saturday", calories: 2280 },
+      { day: "sunday", calories: 2610 }
+    ])
+  );
 
+  console.log(
+  calcAverageCalories([])
+); 
 
 /* Напиши функцію calcAverageCalories(days), 
 яка повертає середньодобове значення кількості калорій, 
